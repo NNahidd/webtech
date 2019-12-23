8fc8cdb1286fc7dff5dd4c1b48bc17184ce091a4
NNahidd <59170042+NNahidd@users.noreply.github.com>
2019-12-23 20:35:26 +0600

    Add files via upload

---
+++ booktickets.php
@@ -0,0 +1,14 @@
+<form name="schedule">
+<table>
+<tr align="center"><td >Country</td><td>State</td><td>City</td><td>Area</td></tr>
+<tr><td id="Country"></td><td id="State"></td><td id="City"></td><td id="Cinema"></td></tr>
+<tr><td>Movies</td><td id="Film" colspan="3"></td></tr>
+<tr><td id="Filmdesc" colspan="4"></td></tr>
+<tr><td>Date</td><td id="Date" colspan="3"></td></tr>
+<tr><td>Time Slot</td><td id="Time" colspan="3"></td></tr>
+<tr><td>Select seats</td><td id="Seats" colspan="3"></td></tr>
+<tr>
+	<td colspan="4" align="center"><input type="button" onclick="bookTicket();" class="customButton" value="Book ticket" /></td>
+</tr>
+</table>
+</form>
